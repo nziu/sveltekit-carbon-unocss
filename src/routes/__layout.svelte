fe423bef71bdf544480237453bd4c2946a664528
Tomoyo <nziu.z@outlook.com>
2022-07-05 15:16:18 +0800

    chore(deps): configure carbon

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import 'carbon-components-svelte/css/all.css';
+
+  import { Content } from 'carbon-components-svelte';
+</script>
+
+<Content>
+  <slot />
+</Content>
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,31 @@
 import adapter from '@sveltejs/adapter-static';
+import { optimizeCss, optimizeImports } from 'carbon-preprocess-svelte';
 import sveltePreprocess from 'svelte-preprocess';
+
+const production = process.env.NODE_ENV === 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [sveltePreprocess()],
+  preprocess: [sveltePreprocess(), optimizeImports()],
 
   kit: {
     adapter: adapter({
       fallback: 'index.html'
-    })
+    }),
+
+    vite: {
+      plugins: [
+        production &&
+          optimizeCss({
+            // https://purgecss.com/safelisting.html
+            safelist: {
+              standard: ['theme']
+            }
+          })
+      ]
+    }
   }
 };
 
