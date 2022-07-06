ac7736760c72437ae49f0a84281cba9da914f909
Tomoyo <nziu.z@outlook.com>
2022-07-06 17:35:31 +0800

    feat(layout): error pages

---
+++ src/routes/__error.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit';
+
+  export const load: Load = ({ error, status }) => {
+    return {
+      props: {
+        title: `${status}: ${error?.message}`
+      }
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let title: string;
+</script>
+
+<h1>{title}</h1>
+<div>
+  Page not found.
+  <a href="/">Return home</a>
+</div>
