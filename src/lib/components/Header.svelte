5dfc0496b517b2de63d56568ce97c24981f799ad
Tomoyo <nziu.z@outlook.com>
2022-07-25 17:52:31 +0800

    refactor(components): header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import { logout } from '$lib/common';
   import {
+    Button,
     Header,
     HeaderAction,
     HeaderGlobalAction,
@@ -9,61 +11,80 @@
     HeaderPanelLink,
     HeaderPanelLinks,
     HeaderUtilities,
-    Modal,
+    Popover,
+    SideNav,
+    SideNavItems,
+    SideNavLink,
     SkipToContent,
     Theme
   } from 'carbon-components-svelte';
-  import { Asleep, Awake, UserAvatar } from 'carbon-icons-svelte';
+  import { Asleep, Awake, Logout, UserAvatar } from 'carbon-icons-svelte';
 
-  const links = [
-    { url: 'https://m.jianlingguo.cn/', desc: '企业邮箱' },
-    { url: 'https://im.jinritemai.com/', desc: '飞鸽下载' },
-    { url: 'https://fxg.jinritemai.com/', desc: '抖店后台' }
+  const PanelLinks = [
+    { href: 'https://m.jianlingguo.cn/', text: '企业邮箱' },
+    { href: 'https://im.jinritemai.com/', text: '飞鸽下载' },
+    { href: 'https://fxg.jinritemai.com/', text: '抖店后台' }
   ];
+  const NavLinks = [
+    { href: '/monitor', text: '监控' },
+    { href: '/manage', text: '管理' }
+  ];
+
+  let isPopoverOpen = false;
+  let isSideNavOpen = false;
 
   const themes = { white: Awake, g90: Asleep };
   let theme: 'white' | 'g90' = 'white';
   const toggle = () => {
     theme !== 'white' ? (theme = 'white') : (theme = 'g90');
   };
-
-  let open = false;
 </script>
 
 <Theme persist bind:theme />
 
-<Header href="/" platformName="Monitor">
+<Header
+  href="/"
+  platformName="Monitor"
+  bind:isSideNavOpen
+  expandedByDefault={false}
+>
   <svelte:fragment slot="skip-to-content">
     <SkipToContent />
   </svelte:fragment>
   <HeaderNav>
-    <HeaderNavItem href="/monitor" text="监控" />
-    <HeaderNavItem href="/settings" text="管理" />
+    {#each NavLinks as { href, text }}
+      <HeaderNavItem {href} {text} />
+    {/each}
   </HeaderNav>
   <HeaderUtilities>
     <HeaderGlobalAction icon={themes[theme]} on:click={toggle} />
-    <HeaderGlobalAction icon={UserAvatar} on:click={() => (open = true)} />
+    <HeaderGlobalAction
+      icon={UserAvatar}
+      on:click={() => (isPopoverOpen = !isPopoverOpen)}
+    />
     <HeaderAction>
       <HeaderPanelLinks>
         <HeaderPanelDivider>相关链接</HeaderPanelDivider>
-        {#each links as { url, desc }}
-          <HeaderPanelLink href={url} target="_blank">{desc}</HeaderPanelLink>
+        {#each PanelLinks as { href, text }}
+          <HeaderPanelLink {href} target="_blank">{text}</HeaderPanelLink>
         {/each}
       </HeaderPanelLinks>
     </HeaderAction>
   </HeaderUtilities>
+  <Popover
+    bind:open={isPopoverOpen}
+    closeOnOutsideClick
+    align="bottom-right"
+    class="pt-1px"
+  >
+    <Button icon={Logout} kind="secondary" on:click={logout}>登出</Button>
+  </Popover>
 </Header>
 
-<Modal
-  size="xs"
-  bind:open
-  modalHeading="登出"
-  primaryButtonText="确定"
-  secondaryButtonText="取消"
-  on:click:button--secondary={() => (open = false)}
-  on:open
-  on:close
-  on:submit
->
-  <p>退出当前帐号</p>
-</Modal>
+<SideNav bind:isOpen={isSideNavOpen}>
+  <SideNavItems>
+    {#each NavLinks as { href, text }}
+      <SideNavLink {href} {text} />
+    {/each}
+  </SideNavItems>
+</SideNav>
