a030df83d37e62b6221308ebe8f0efc8472e0b7e
Tomoyo <nziu.z@outlook.com>
2022-07-29 13:39:42 +0800

    feat(routes): login

--- src/routes/__layout-reset.svelte
+++ src/routes/__layout-reset.svelte
@@ -1,3 +1,17 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit';
+  import { getItem } from '$lib/common';
+
+  export const load: Load = ({ url }) => {
+    if (getItem('token') && url.pathname === '/login') {
+      return {
+        status: 302,
+        redirect: '/'
+      };
+    }
+  };
+</script>
+
 <script lang="ts">
   import { Content } from 'carbon-components-svelte';
 </script>
--- src/routes/login@reset.svelte
+++ src/routes/login@reset.svelte
@@ -1,25 +1,32 @@
-<script context="module" lang="ts">
-  import type { Load } from '@sveltejs/kit';
-  import { getItem } from '$lib/common';
-
-  export const load: Load = ({ url }) => {
-    if (getItem('token') && url.pathname === '/login') {
-      return {
-        status: 302,
-        redirect: '/'
-      };
-    }
-  };
-</script>
-
 <script lang="ts">
   import { goto } from '$app/navigation';
+  import { login } from '$lib/api';
   import { setItem } from '$lib/common';
-  import { Button, Theme } from 'carbon-components-svelte';
+  import type { LoginInput } from '$lib/types';
+  import {
+    Button,
+    FluidForm,
+    InlineNotification,
+    PasswordInput,
+    TextInput,
+    Theme
+  } from 'carbon-components-svelte';
+  import { ArrowRight } from 'carbon-icons-svelte';
 
-  async function submit() {
-    setItem('token', 'test');
-    goto('/');
+  let showNotification = false;
+  const form: LoginInput = {
+    username: '',
+    password: ''
+  };
+
+  async function submit(e: Event) {
+    e.preventDefault();
+    try {
+      setItem('token', await login(form));
+      goto('/');
+    } catch (error) {
+      showNotification = true;
+    }
   }
 </script>
 
@@ -29,4 +36,41 @@
 
 <Theme theme="g10" />
 
-<Button on:click={submit}>Login</Button>
+<div class="flex items-center justify-center h-70vh">
+  <div class="bg-white shadow-md w-md">
+    <div class="m-4">
+      <h1>登录</h1>
+      <p class="text-gray-500">店铺数据监控</p>
+      {#if showNotification}
+        <InlineNotification
+          subtitle="帐号或密码错误"
+          on:close={() => (showNotification = false)}
+        />
+      {/if}
+    </div>
+    <FluidForm on:submit={submit}>
+      <TextInput
+        required
+        labelText="帐号"
+        placeholder="请输入帐号"
+        bind:value={form.username}
+      />
+      <PasswordInput
+        required
+        type="password"
+        labelText="密码"
+        placeholder="请输入密码"
+        bind:value={form.password}
+      />
+      <Button
+        size="lg"
+        type="submit"
+        kind="secondary"
+        icon={ArrowRight}
+        class="w-full !max-w-full mt-8"
+      >
+        登录
+      </Button>
+    </FluidForm>
+  </div>
+</div>
